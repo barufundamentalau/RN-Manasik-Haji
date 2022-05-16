bbe9f77582e052678b06b7eaea9a2ae190c7a351
capadesu <blackz.wd@gmail.com>
2022-05-16 18:24:39 +0800

    style home

--- src/components/molecules/LogoSimanhto/index.js
+++ src/components/molecules/LogoSimanhto/index.js
@@ -1,14 +1,25 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
+import {Logo} from '../../../assets';
 
 const LogoSimanhto = () => {
   return (
-    <View>
-      <Text>Logo Simanhto</Text>
+    <View style={styles.container}>
+      <Logo />
+      <Text style={styles.title}>Si Manhto</Text>
     </View>
   );
 };
 
 export default LogoSimanhto;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  title: {fontSize: 22, fontFamily: 'Poppins-Medium', color: '#FFFF'},
+  container: {
+    alignItems: 'center',
+    backgroundColor: '#28925A',
+    paddingHorizontal: 24,
+    paddingTop: 26,
+    paddingBottom: 30,
+  },
+});
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,13 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {Button, Gap} from '../../components';
+import {Button, Gap, LogoSimanhto} from '../../components';
 
 const Home = () => {
   return (
     <View style={styles.page}>
-      <Text>Home Page Si Manhto</Text>
+      <View>
+        <LogoSimanhto />
+      </View>
       <View style={styles.container}>
         <Gap height={14} />
         <Button text="Pedoman Manasik Haji dan Umroh" />
@@ -21,14 +23,13 @@ const Home = () => {
 };
 
 export default Home;
-
 const styles = StyleSheet.create({
   page: {flex: 1},
   container: {
     backgroundColor: 'yellow',
     paddingHorizontal: 24,
     paddingVertical: 26,
-    marginTop: 24,
+    marginTop: 0,
     flex: 1,
   },
 });
